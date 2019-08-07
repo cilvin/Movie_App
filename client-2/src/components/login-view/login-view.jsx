7214f289daf246789c2f4d93295c7411486be077
cilvin <cilvin33@users.noreply.github.com>
2019-08-06 18:50:32 -0700

    Add login-view authentication
    Require username, password, and jwt token.

--- client-2/src/components/login-view/login-view.jsx
+++ client-2/src/components/login-view/login-view.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 import './login-view.scss';
 
 export function LoginView(props) {
@@ -9,12 +10,20 @@ export function LoginView(props) {
     const [ password, setPassword ] = useState('');
 
     const handleSubmit = (e) => {
-        e.preventDefault(); 
-        console.log(username, password);
+        e.preventDefault();
         /* Send a request to the server for authentication */
-        /* then call props.onLoggedIn(username) */
-        props.onLoggedIn(username);
-    };
+        axios.post('https://floating-ocean-36499.herokuapp.com//login', {
+          Username: username,
+          Password: password
+        })
+        .then(response => {
+          const data = response.data;
+          props.onLoggedIn(data);
+        })
+        .catch(e => {
+          console.log('no such user')
+        });
+      };
 
     return (
         <Form>
