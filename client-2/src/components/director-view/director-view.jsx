202de47ee269001d9ab3b35cf331b985df34dce1
cilvin <cilvin33@users.noreply.github.com>
2019-08-11 15:19:44 -0700

    Add director view

---
+++ client-2/src/components/director-view/director-view.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import './director-view.scss';
+import { Link } from 'react-router-dom';
+
+
+export class DirectorView extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {};
+    }
+     render() {
+         const { director } = this.props;
+
+         if (!director) return null;
+
+         return (
+             <div className='director-view'>
+                <h1 className='director'>{director.Name}</h1>
+                <h2>Biography</h2>
+                <div className='bio'>{director.Bio}</div>
+                <h2>Born</h2>
+                <div className='birth'>{director.Birth}</div>
+                <h2>Died</h2>
+                <div className='death'>{director.Death}</div>
+                <Link to={'/'}>
+                    <Button variant='outline-dark' >Back</Button>
+                </Link>
+                
+             </div>
+         );
+     }
+     
+}
+
+DirectorView.propTypes = {
+    Director: PropTypes.shape({
+        Name: PropTypes.string,
+        Bio: PropTypes.string,
+        Death: PropTypes.string
+    }).isRequired,
+    onClick: PropTypes.func.isRequired
+};
