04c251ed0fd61cc9efb1b57d6aa6d4fca5118838
cilvin <cilvin33@users.noreply.github.com>
2019-08-16 17:49:43 -0700

    Add movie DB file

---
+++ client-2/src/movieDB/movie-DB.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+// OMDb API Key
+// const api = 'http://www.omdbapi.com/?19aaf52d&';
+// https://developers.themoviedb.org/3/search/search-movies
+
+const apiKey = process.env.e23e5607a1801b5693cf7ee99ff11da0;
+
+const checkStatus = response => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    const error = new Error(`HTTP Error ${response.statusText}`);
+    error.status = response.statusText;
+    error.response = response;
+    console.log(error);
+    throw error;
+  }
+};
+
+const theMovieDBSearch = title =>
+  axios.get(
+    `https://api.themoviedb.org/3/movie/550?api_key=${apiKey}&language=en-US&query=${title}&page=1&include_adult=false`
+  );
+
+const poster = {
+  loadSize: (size = 'w300', posterPath = '') => {
+    return `http://image.tmdb.org/t/p/${size}${posterPath}`;
+  },
+  loadOriginalSize: (posterPath = '') => {
+    return `http://image.tmdb.org/t/p/original${posterPath}`;
+  }
+};
+
+export { theMovieDBSearch, poster, checkStatus };
