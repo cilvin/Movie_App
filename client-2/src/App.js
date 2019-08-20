6660f249a97c6cfa1df8931cf7206487c70e74b4
cilvin <cilvin33@users.noreply.github.com>
2019-08-20 11:31:02 -0700

    3.6 Wrap main-view in provider
    Import reducers. Create store.

--- client-2/src/App.js
+++ client-2/src/App.js
@@ -1,14 +1,22 @@
 // App.js
 import React, { Component } from 'react';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
 
-import { MainView } from './components/main-view/main-view';
+import moviesApp from './reducers/reducers';
+import  MainView  from './components/main-view/main-view';
 
 import './App.css';
+
+const store = createStore(moviesApp);
+
 
 class App extends Component {
   render() {
     return (
-      <MainView/>
+      <Provider store={store}>
+        <MainView/>
+      </Provider>
     );
   }
 }
