5cc1a7fbe1080f584b9040ad0a4fda3d05589755
cilvin <cilvin33@users.noreply.github.com>
2019-08-20 11:33:37 -0700

    3.6 Add Redux logic to movie-view

--- client-2/src/components/movie-view/movie-view.jsx
+++ client-2/src/components/movie-view/movie-view.jsx
@@ -1,48 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import './movie-view.scss';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { connect } from 'react-redux';
 
 
-export class MovieView extends React.Component {
-    constructor() {
-        super();
-        
-        this.state = {
-            testingUrl: []
-        };
-    }
+function MovieView(props) {
+    const { movies, movieId } = props;
 
-    componentDidMount() {
-        this.searchMovieDB();
+    if (!movies || !movies.length) return null;
 
-        }
-       
-        
-        
+    const movie = movies.find(movie => movie._id == movieId);
 
-    searchMovieDB() {
-        const  movie   = this.props;
-        let apiKey= 'e23e5607a1801b5693cf7ee99ff11da0';
-        let apiUrl= `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movie.Title}&page=1&include_adult=false`;
-         axios.get(apiUrl).then(response => {
-            // response.status(200).json(response.data);
-             console.log(response.data);
-           //  this.setState({
-            //     testingUrl: response.data
-             })
-             
-            // })          
-        }
-
-
-   
-
-    submitLike(event) {
+    function submitLike(event) {
         event.preventDefault();
-        const { movie } = this.props;
         let userEndpoint = 'https://floating-ocean-36499.herokuapp.com/users/';
         let usernameLocal = localStorage.getItem('user');
         let token = localStorage.getItem('token');
@@ -53,7 +25,6 @@ export class MovieView extends React.Component {
           headers: { Authorization: `Bearer ${token}`}
         })
         .then(response => {
-            
           console.log(response);
           alert('Movie has been added to your Favorite List!');
           //update localStorage
@@ -65,54 +36,79 @@ export class MovieView extends React.Component {
         });
       };
 
-     render() {
-         const { movie } = this.props;
+
+
+    return (
+        <div className='movie-view'>
+            <div className='movie-title'>
+                <h2 className='label'>Title</h2>
+                <p className='value'>{movie.Title}</p>
+            </div>
+            <div className='movie-description'>
+                <h3 className='label'>Description</h3>
+                <p className='value'>{movie.Description}</p>
+            </div>
+            <img alt='' className='movie-poster'  />
+        
+    
+
+            <div className='movie-genre'>
+                <Link to={`/genre/${movie.Genre.Name}`}>
+                    <h3 className='label'>Genre</h3>
+                    <Button variant='outlin-dark'><p className='value'>{movie.Genre.Name}</p></Button>
+                </Link>
+            </div>
+            <div className='movie-director'>
+                <Link to={`/director/${movie.Director.Name}`}>
+                    <h3 className='label'>Director</h3>
+                </Link>
+                <h4>{movie.Director.Name}</h4>
+            </div>
+            <Link to={'/'}>
+                <Button variant='outline-dark' >Back</Button>
+            </Link>
+        
+            <Button variant='outline-dark' onClick={event => submitLike(event)}>Like</Button>
+     </div>
+
+    );
+}
+
+export default connect(({movies}) => ({movies}))(MovieView);
+
+//Old code below
+
+   // componentDidMount() {
+      //  this.searchMovieDB();
+
+      //  }
+       
+        
+        
+
+   // searchMovieDB() {
+     //   const  movie   = this.props;
+    //    let apiKey= 'e23e5607a1801b5693cf7ee99ff11da0';
+    //    let apiUrl= `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${movie.Title}&page=1&include_adult=false`;
+    //     axios.get(apiUrl).then(response => {
+            // response.status(200).json(response.data);
+        //     console.log(response.data);
+           //  this.setState({
+            //     testingUrl: response.data
+     //        })
+             
+            // })          
+     //   }
+
+
+   
+
+    
+
+     
          
          //const imgUrl= `https://image.tmdb.org/t/p/w500/dM2w364MScsjFf8pfMbaWUcWrR.jpg`;
         
 
-         if (!movie) return null;
 
-         return (
-             <div className='movie-view'>
-                <div className='movie-title'>
-                    <h2 className='label'>Title</h2>
-                    <p className='value'>{movie.Title}</p>
-                </div>
-                <div className='movie-description'>
-                    <h3 className='label'>Description</h3>
-                    <p className='value'>{movie.Description}</p>
-                </div>
-                <img alt='' className='movie-poster'  />
-                <p>{this.state.testingUrl}</p>
-            
-
-                <div className='movie-genre'>
-                    <Link to={`/genre/${movie.Genre.Name}`}>
-                        <h3 className='label'>Genre</h3>
-                        <Button variant='outlin-dark'><p className='value'>{movie.Genre.Name}</p></Button>
-                    </Link>
-                </div>
-                <div className='movie-director'>
-                    <Link to={`/director/${movie.Director.Name}`}>
-                        <h3 className='label'>Director</h3>
-                    </Link>
-                    <h4>{movie.Director.Name}</h4>
-                </div>
-                <Link to={'/'}>
-                <Button variant='outline-dark' >Back</Button>
-                </Link>
-                
-                <Button variant='outline-dark' onClick={event => this.submitLike(event)}>Like</Button>
-             </div>
-         );
-     }
-     
-}
-
-//MovieView.propTypes = {
-  //  movie: PropTypes.shape({
- //       Title: PropTypes.string,
- //   }).isRequired,
    
-//};
