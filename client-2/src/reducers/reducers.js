61763d4eebbc90fdf38fd0f86783ec1bc4570fdc
cilvin <cilvin33@users.noreply.github.com>
2019-08-20 11:37:08 -0700

    3.6 Add reducers to describe how the state has changed

---
+++ client-2/src/reducers/reducers.js
@@ -0,0 +1,48 @@
+import { combineReducers } from 'redux';
+
+import { SET_FILTER, SET_SORT_COLUMN, SET_MOVIES, SET_LOGGEDIN_USER } from '../actions/actions';
+
+function visibilityFilter(state = '', action) {
+    switch (action.type) {
+        case SET_FILTER:
+            return action.value;
+        default:
+            return state;
+    }
+}
+
+function sortColumn(state = 'title', action) {
+    switch (action.type) {
+        case SET_SORT_COLUMN:
+            return action.value;
+        default:
+            return state;
+    }
+}
+
+function movies(state = [], action) {
+    switch (action.type) {
+        case SET_MOVIES:
+            return action.value;
+        default:
+            return state;
+    }
+}
+
+function loggedInUser(state = [], action) {
+    switch (action.type) {
+        case SET_LOGGEDIN_USER:
+            return action.value;
+        default:
+            return state;
+    }
+}
+
+const moviesApp = combineReducers({
+    visibilityFilter,
+    sortColumn,
+    movies,
+    loggedInUser
+});
+
+export default moviesApp;
