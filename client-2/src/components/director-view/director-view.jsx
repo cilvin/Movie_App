a01007985d89018a05187c1521589dd92cf69b24
cilvin <cilvin33@users.noreply.github.com>
2019-08-20 11:31:27 -0700

    3.6 Use redux logic in director view

--- client-2/src/components/director-view/director-view.jsx
+++ client-2/src/components/director-view/director-view.jsx
@@ -3,37 +3,34 @@ import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import './director-view.scss';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
-export class DirectorView extends React.Component {
-    constructor() {
-        super();
+function DirectorView(props) {
+    const { movies, directorName } = props;
 
-        this.state = {};
-    }
-     render() {
-         const { director } = this.props;
+    if (!movies || !movies.length) return null;
 
-         if (!director) return null;
+    const director = movies.find(movie => movie.Director.Name === directorName).Director;
 
-         return (
-             <div className='director-view'>
-                <h1 className='director'>{director.Director.Name}</h1>
-                <h2>Biography</h2>
-                <div className='bio'>{director.Director.Bio}</div>
-                <h2>Born</h2>
-                <div className='birth'>{director.Director.Birth}</div>
-                <h2>Died</h2>
-                <h5 className='death'>{director.Director.Death}</h5>
-                <Link to={'/'}>
-                    <Button variant='outline-dark' >Back</Button>
-                </Link>
-                
-             </div>
-         );
-     }
-     
+    return (
+        <div className='director-view'>
+           <h1 className='director'>{director.Name}</h1>
+           <h2>Biography</h2>
+           <div className='bio'>{director.Bio}</div>
+           <h2>Born</h2>
+           <div className='birth'>{director.Birth}</div>
+           <h2>Died</h2>
+           <h5 className='death'>{director.Death}</h5>
+           <Link to={'/'}>
+               <Button variant='outline-dark' >Back</Button>
+           </Link>
+           
+        </div>
+    );
 }
+
+export default connect(({movies}) => ({movies}))(DirectorView);
 
 DirectorView.propTypes = {
     Director: PropTypes.shape({
